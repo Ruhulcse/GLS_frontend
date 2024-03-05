940ec8ec67be7ab781ddf3e95454f5a07f198268
Faysal Ahmed Bappy <faysalahmed.ces.98@gmail.com>
2024-03-06 00:33:52 +0600

    blog main page some problem solve

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-
+import { Link } from 'react-router-dom'
 function Blog({item}) {
   
     const splitText = (text,size) =>{
@@ -17,8 +17,8 @@ function Blog({item}) {
             <img src={item.image} className='h-full w-full object-fill' alt="" />
         </div>
         <div className='mx-6'>
-            <p className='text-xl font-medium py-8'>{splitText(item.info,150)}</p>
-            <p  className='uppercase text-xl font-bold text-blue-700 cursor-pointer mb-12 mt-[-6px]'>read more</p>
+            <Link to={`/blog/${item.id}`} state={item} className='text-xl font-medium py-8'>{splitText(item.info,150)}</Link>
+            <p  className='uppercase text-xl font-bold text-blue-700 cursor-pointer mb-12 mt-[24px]'>read more</p>
         </div>
     </div>
   )
