50dd0d1aec858de9cefcd4ba878cad77c2e33b5a
Kawser365 <mdkamrulhasan.khk@gmail.com>
2024-03-05 14:50:12 +0600

    navbar update hero section done and service section done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import backgroundImage from '../../assets/home/truck.jpg'
-import Container from '../../shared/Container/Container'
+import React from "react";
+import backgroundImage from "../../assets/home/truck.jpg";
+import Container from "../../shared/Container/Container";
 const Hero = () => {
   return (
-    <section 
-    className='mt-[88px] bg-cover bg-center'
-    style={{backgroundImage: `url(${backgroundImage})`, objectFit:'cover'}}
+    <section
+      className="mt-[88px] bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundImage})`, objectFit: "cover" }}
     >
-    <div className='bg-gradient-to-r from-[#001427] w-full sm:h-screen h-[70vh]'>
-      <div className='relative sm:top-1/4 top-[20%] flex flex-col space-y-4'>
-        <Container>
-        <h1 className='uppercase sm:text-6xl text-3xl font-bold text-white sm:leading-tight' data-aos="fade-right">Fast & Safest <br /> Shipping service</h1>
-        <p className='text-base text-white font-normal leading-normal tracking-normal my-6'>At first, we ensure the product safety. From starting product loading to unloading <br className='hidden sm:block' /> we maintain the standard terms of safety while in transit.</p>
-        <div>
-        <button className='px-8 py-6 text-sm tracking-normal uppercase font-semibold text-white bg-[#03045e] hover:bg-[#0B0D7D] transition-all duration-500' data-aos="fade-left">Request a quote</button>
+      <div className="bg-gradient-to-r from-[#001427] w-full sm:h-screen h-[80vh]">
+        <div className="relative top-1/4 flex flex-col space-y-4">
+          <Container>
+            <h1
+              className="uppercase sm:text-6xl text-3xl font-bold text-white sm:leading-tight"
+              data-aos="fade-right"
+            >
+              Fast & Safest <br /> Shipping service
+            </h1>
+            <p className="text-base text-white font-normal leading-normal tracking-normal my-6">
+              At first, we ensure the product safety. From starting product
+              loading to unloading <br className="hidden sm:block" /> we
+              maintain the standard terms of safety while in transit.
+            </p>
+            <div>
+              <button
+                className="px-8 py-6 text-sm tracking-normal uppercase font-semibold text-white bg-[#03045e] hover:bg-[#0B0D7D] transition-all duration-500"
+                data-aos="fade-left"
+              >
+                Request a quote
+              </button>
+            </div>
+          </Container>
         </div>
-        </Container>
       </div>
-    </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
