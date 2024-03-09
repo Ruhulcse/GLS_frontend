d4c6362d3bad45b125514830966e19d080bdb077
ruhulcse <ruhul.cse7862@gmail.com>
2024-03-09 22:34:08 +0600

    blog section feedback

--- src/components/blog/Blogs.jsx
+++ src/components/blog/Blogs.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { data } from '.'
-import Blog from './Blog'
+import { data } from ".";
+import Blog from "./Blog";
 
 function Blogs() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-8 mx-4 xl:grid-cols-3 mt-24'>
-        {data.map((item,i)=>(
-            <Blog key={i} item={item} />
-        ))}
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-4 xl:grid-cols-3 mt-24">
+      {data.map((item, i) => (
+        <Blog key={i} item={item} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Blogs
+export default Blogs;
