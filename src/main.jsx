22692927e3e9721cb745ab58054791d9e1775083
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-03-15 23:39:26 +0600

    add redux, add config, abs imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './app/store.js';
+
 Aos.init({
-  duration:1500
+    duration: 1500,
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+);
