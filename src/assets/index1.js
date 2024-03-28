113a0d293e59d866f2870bc9edd36f937b5481bf
Faysal Ahmed Bappy <faysalahmed.ces.98@gmail.com>
2024-03-29 00:34:20 +0600

    Guide data update

---
+++ src/assets/index1.js
@@ -0,0 +1,7 @@
+import callCenter from './guide/call-center-8643475_1280.jpg'
+import bus from './guide/GIFALCzWoAAh6xI.jpeg'
+import callCenter1 from './guide/hello.jpeg'
+import bus1 from './guide/how-to-become-a-shipper-of-choice-blog-hero-image-1200x675px.jpg'
+//import driver from './guide/Untitled6.jpeg'
+
+export {callCenter,callCenter1,bus,bus1}
