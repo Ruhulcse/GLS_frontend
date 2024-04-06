6eb2b75894fce9997e2e5bc8feea5012e78b6e44
Faysal Ahmed Bappy <faysalahmed.ces.98@gmail.com>
2024-04-06 16:42:42 +0600

    auth page  complete using RFH

--- src/auth/SignUp.jsx
+++ src/auth/SignUp.jsx
@@ -5,48 +5,52 @@ import logo from "./../assets/home/logo.png";
 import { formFiled } from "./form";
 import { useNavigate } from "react-router-dom";
 import { useRegisterUserMutation } from "@/store/api/auth/authApiSlice";
+import Textinput from "@/components/ui/Textinput";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Select from "@/components/ui/Select";
+import useToast from "@/hooks/useToast";
+import Button from "@/components/ui/Button";
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const schema = yup.object({
+  firstName: yup.string().label("First Name").required(),
+  lastName: yup.string().label("Last Name").required(),
+  email: yup.string().email("invalid email").required("email is required"),
+  phoneNumber: yup.string().matches(phoneRegExp, "Phone number is not valid"),
+  password: yup.string().required(),
+  confirmPassword: yup
+    .string()
+
+    .oneOf([yup.ref("password"), null], "Passwords must match!"),
+  userType: yup
+    .string()
+    .oneOf(["agent", "carrier", "broker", "shipper"])
+    .required("select user type"),
+});
 
 function SignUp() {
-  const [formState, setFormState] = useState(transferObj(formFiled));
-  const [category,setCategory] = useState('')
-  const formData = objectToArray(formState);
-  const [error,setError] = useState('')
-  const [register,{data,isLoading,error:responseError}] = useRegisterUserMutation()
-  const navigate = useNavigate()
+  const { errorToast } = useToast();
+  const [SignUp, { data, isLoading, error: responseError }] =
+    useRegisterUserMutation();
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormState({
-      ...formState,
-      [name]: {
-        ...formState[name],
-        value: value,
-      },
-    });
-  };
-  const handleCategory =(e)=>{
-    setCategory(e.target.value)
-  }
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "all",
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const {fName,lName,email,password,phone,confirmPassword} = formState
-    if(password.value!==confirmPassword.value)
-    {
-      setError('passwords do not match')
-    }
-    else {
-      register({
-        firstName:fName.value,
-        lastName:lName.value,
-        email:email.value,
-        password:password.value,
-        category,
-        phoneNumber:phone.value
-      })
-      setFormState(transferObj(formFiled))
-      setCategory('')
-      navigate('/logIn')
+  const onSubmit = async (data) => {
+    try {
+      await SignUp(data).unwrap();
+      navigate("/logIn");
+    } catch (error) {
+      errorToast(error);
     }
   };
 
@@ -58,7 +62,7 @@ function SignUp() {
         <div className="mt-12 h-full w-full flex justify-center items-center">
           <form
             className="bg-gradient-to-tl from-[#afc0ee] to-[#6d5cf0] rounded-xl  min-h-96 w-96 lg:w-[420px] shadow-xl"
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
           >
             <div className="mb-12 grid justify-center">
               <img
@@ -67,42 +71,109 @@ function SignUp() {
                 alt=""
               />
 
-              {formData.map((item) => (
-                <div className="flex flex-col my-px">
-                  <label htmlFor="" className="text-base
-                   font-medium my-[2px]">
-                    {item.title}
-                  </label>
-                  <input
-                    type={item.type}
-                    placeholder={item.placeholder}
-                    name={item.name}
-                    onChange={handleChange}
-                    value={item.value}
-                    className="w-64 lg:w-72 px-2 p-1.5 rounded-lg"
-                  />
-                </div>
-              ))}
-              {error&& <div className="text-red-600">{error}</div>}
-               <label className="text-base font-medium my-[2px]">Category:</label>
-                <select value={category} onChange={handleCategory}name="category">
-                  <option value="">category</option>
-                  <option value="carriers">Carriers</option>
-                  <option value="brokers">Brokers</option>
-                  <option value="shippers">Shippers</option>
-                </select>
-
-              <button
+              <Textinput
+                label="First Name"
+                type="text"
+                name="firstName"
+                error={errors.firstName}
+                defaultValue=""
+                placeholder="Enter your first name"
+                register={register}
+                className="w-[15rem]"
+                autoComplete="firstName"
+              />
+              <Textinput
+                label="Last Name"
+                type="text"
+                name="lastName"
+                defaultValue=""
+                error={errors.lastName}
+                autoComplete="lastName"
+                placeholder="Enter your last name"
+                register={register}
+                className="w-[15rem]"
+              />
+              <Textinput
+                label="Email"
+                type="email"
+                name="email"
+                defaultValue=""
+                error={errors.email}
+                autoComplete="email"
+                placeholder="Enter your email"
+                register={register}
+                className="w-[15rem]"
+              />
+              <Textinput
+                label="Phone"
+                placeholder="Phone"
+                type="tel"
+                name="phoneNumber"
+                error={errors.phoneNumber}
+                autoComplete="phone"
+                register={register}
+                className="w-[15rem]"
+              />
+              <Textinput
+                label="Password"
+                placeholder="Password"
+                type="password"
+                name="password"
+                autoComplete="password"
+                error={errors.password}
+                register={register}
+                className="w-[15rem]"
+              />
+              <Textinput
+                label="Confirm Password"
+                type="password"
+                name="confirmPassword"
+                autoComplete="confirmPassword"
+                error={errors.confirmPassword}
+                placeholder="Confirm password"
+                register={register}
+                className="w-[15rem]"
+              />
+              <Select
+                label="User type"
+                defaultValue=""
+                options={["agent", "shipper", "broker", "carrier"]}
+                name="userType"
+                register={register}
+                error={errors.userType}
+                placeholder="Select User type"
+              />
+              {/* <button
                 className="inline-block mx-auto mt-6 bg-gray-200 w-32 text-center rounded-full p-2 cursor-pointer hover:bg-black hover:text-white transition-all hover:bg-black-500"
                 type="submit"
+                isLoading={isLoading}
               >
                 Sign Up
-              </button>
+              </button> */}
+              <Button
+                type="submit"
+                text="Sign Up"
+                className="btn btn-dark block w-full text-center mt-4"
+                isLoading={isLoading}
+              />
+              {/* <Button
+                type="submit"
+                text="Sign in"
+                className="btn btn-dark block w-full text-center mt-4"
+                isLoading={isLoading}
+              /> */}
             </div>
-          <div className="flex justify-between mb-4 mx-6 mt-[-12px] items-center">
-            <div className="text-base text-blue-950 font-bold">Do you have an account?</div>
-            <div className="text-xl font-bold cursor-pointer hover:text-black-500" onClick={()=>navigate('/logIn')}>Sign In</div>
-          </div>
+            <div className="flex justify-between mb-4 mx-6 mt-[-12px] items-center">
+              <div className="text-base text-blue-950 font-bold">
+                Do you have an account?
+              </div>
+              <div
+                className="text-xl font-bold cursor-pointer hover:text-black-500"
+                onClick={() => navigate("/logIn")}
+              >
+                Sign In
+              </div>
+            </div>
           </form>
         </div>
       </div>
