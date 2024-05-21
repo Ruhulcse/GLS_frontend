601dacba48eb4615eea2d2bfeef940449d6d1bc5
ruhulcse <ruhul.cse7862@gmail.com>
2024-05-22 00:08:36 +0600

    strick off

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,26 @@
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import Aos from "aos";
+import "aos/dist/aos.css";
 import "flatpickr/dist/themes/light.css";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import 'react-toastify/dist/ReactToastify.css';
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
 import "simplebar-react/dist/simplebar.min.css";
 import "../src/assets/scss/app.scss";
-import App from './App.jsx';
-import './index.css';
-import store from './store';
+import App from "./App.jsx";
+import "./index.css";
+import store from "./store";
 // import { blogsApi } from './store/api/blogs/blogsApi.js';
 
 Aos.init({
-	duration: 1500,
+  duration: 1500,
 });
 
 // store.dispatch(blogsApi.endpoints.getBlogs.initiate());
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </React.StrictMode>
 );
